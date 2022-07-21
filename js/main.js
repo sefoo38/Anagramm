4f6ece865bfbbcf5b13c032a175b9fc0ddb2fadf
sefoo38 <simsek.sefa@gmx.de>
2022-07-21 23:29:06 +0200

    Import Jquery and setup input fields

---
+++ js/main.js
@@ -0,0 +1,30 @@
+$(function (){
+    console.log("Document Ready");
+
+    $('#checkAnagramm').on("click", function() {
+       
+        checkAnagramm();
+    });
+
+});
+
+// First Version 
+
+function checkAnagramm() {
+    let word1 = $('#firstWord').val();
+    let word2 = $('#secondWord').val();
+
+    console.log('First Word: ' + word1 + '\nSecond Word: ' + word2);
+
+    word1Sorted = word1.split('').sort().join('');
+    word2Sorted = word2.split('').sort().join('');
+    
+    console.log('First Word Sorted: ' + word1Sorted + '\nSecond Word Sorted: ' + word2Sorted);
+
+    if (word1Sorted == word2Sorted) {
+        console.log('Anagramm');
+    } else {
+        console.log('Not Anagramm');
+    }
+
+}
